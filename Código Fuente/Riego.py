df803682c646b4543960452fa9ce15d62d8b36bd
Rafa1104 <58662403+Rafa1104@users.noreply.github.com>
2022-01-12 03:04:33 -0500

    Update Riego.py

--- Código Fuente/Riego.py
+++ Código Fuente/Riego.py
@@ -3,8 +3,7 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(8, GPIO.IN) # Dias (incrementa los dias, si llega a 3 se hace el riego)
-#GPIO.setup(22, GPIO.IN) # Sensor de Humedad de Riego
+GPIO.setup(8, GPIO.IN)  # Dias (incrementa los dias, si llega a 3 se hace el riego)
 GPIO.setup(24, GPIO.IN) # Tierra
 GPIO.setup(26, GPIO.IN) # Reserva
 GPIO.setup(28, GPIO.IN) # Estacion
