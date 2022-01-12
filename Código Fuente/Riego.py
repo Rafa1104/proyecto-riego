e8288c3e889627a95a307858a3afc41f150b4198
Rafa1104 <58662403+Rafa1104@users.noreply.github.com>
2022-01-12 02:59:42 -0500

    Clase Riego

--- Código Fuente/Riego.py
+++ Código Fuente/Riego.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(8, GPIO.IN) # Dias (incrementa los dias, si llega a 3 se hace el riego)
 #GPIO.setup(22, GPIO.IN) # Sensor de Humedad de Riego
-GPIO.setup(24, GPIO.IN) # Humedad
+GPIO.setup(24, GPIO.IN) # Tierra
 GPIO.setup(26, GPIO.IN) # Reserva
 GPIO.setup(28, GPIO.IN) # Estacion
 #GPIO.setup(8, GPIO.IN) # Dias
@@ -14,4 +14,23 @@ GPIO.setup(32, GPIO.OUT) #Bomba
 
 
 
-#Class Riego:
+Class Riego:
+  #sensor_tierra
+  #sensor_agua
+  #regar
+  
+  def lectura_sensor(self):
+    
+    # Sensor de Tierra: Tierra seca 1 - Tierra humeda = 0
+    if GPIO.input(24) == 1:
+      tierra = 1
+    else:
+      tierra = 0
+      
+    # Sensor de la Reserva: Reserva llena 1 - Reserva vacia = 0
+    if GPIO.input(26) == 1:
+      agua = 1
+    else:
+      agua = 0
+    
+    return(tierra, agua)
