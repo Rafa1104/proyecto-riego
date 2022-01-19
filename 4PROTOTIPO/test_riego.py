8cb29ed74db8505e8e9ca8b112e14f844db18e9c
Rafa1104 <58662403+Rafa1104@users.noreply.github.com>
2022-01-18 23:05:01 -0500

    Update test_riego.py

--- 4PROTOTIPO/test_riego.py
+++ 4PROTOTIPO/test_riego.py
@@ -1,3 +1,11 @@
 import unittest
 
-import riego
+#import riego
+
+class TestRiego(unittest.TestCase):
+
+    def test_sumaaa(self):
+        self.assertEqual(2+2, 4)
+
+if __name__ == '__main__':
+    unittest.main()
